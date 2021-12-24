3159169cd55866884a8e522fe42193cecb235d2c
hammadpervez <hammadpervez6@gmail.com>
2021-12-24 15:22:04 +0500

    added hour and complete functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_ui/ui/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-  debugShowCheckedModeBanner: fasle,
-      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:const  TextTheme(
+        headline5: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        headline3: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      )),
+      home: const HomeView(),
     );
   }
 }
---
+++ lib/ui/home_view.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int quantity = 0;
+  int mintues = 0;
+  int hour = 0;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), startTimer);
+  }
+
+  void startTimer() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (hasCompletedSecOrMint(quantity)) {
+        quantity = 0;
+        mintues++;
+      } else {
+        ++quantity;
+      }
+      if (hasCompletedSecOrMint(mintues)) ++hour;
+
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: const Color(0xFF2C2C2C),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          _buildSkipRestButton(),
+        ],
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Rest Time', style: Theme.of(context).textTheme.headline5),
+          const SizedBox(height: 5),
+          Text(
+              "${displayHour(hour)} ${showZeroCount(mintues)}$mintues : ${showZeroCount(quantity)}$quantity",
+              style: Theme.of(context).textTheme.headline3),
+        ],
+      )),
+    );
+  }
+
+  TextButton _buildSkipRestButton() {
+    return TextButton(
+        style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(Colors.yellow)),
+        onPressed: () {},
+        child: Row(
+          children: const [
+            Text('Skip Rest'),
+            Icon(Icons.arrow_forward_ios_rounded, size: 20)
+          ],
+        ));
+  }
+
+  String displayHour(int hour) {
+    if (hasHourCompleted) {
+      return showZeroCount(hour) + '$hour :';
+    }
+    return '';
+  }
+
+  bool hasCompletedSecOrMint(int value) => value > 59 ? true : false;
+  bool get hasHourCompleted => mintues > 59 ? true : false;
+  String showZeroCount(int value) => value < 10 ? '0' : '';
+}
